12b922e29b05718d362ecc891614d83f44e3f202
Zhang Jian Nan <jn.zhang610@gmail.com>
2014-10-14 18:30:27 +0800

    add factory & fetch ideas in controller

--- static/controllers/home.js
+++ static/controllers/home.js
@@ -7,6 +7,19 @@
  */
 
 var app = angular.module('IdeaBoard',['akoenig.deckgrid']);
+
+app.factory('IdeasFactory',function($http,$filter,$q){
+    factory.getAllIdeas = function(){
+        var promise = $http.get("http://128.199.160.57:9600/dump")
+            .then(function(response){
+                console.log(response);
+                return response.data;
+            });
+
+        return promise;
+    }
+});
+
 app.controller('HomeController', [
 
     '$scope',
@@ -22,6 +35,16 @@ app.controller('HomeController', [
             {content:'hahaha',title:'vvvv'}
         ];
 
+    }
+
+    $scope.FetchAllIdeas = function(){
+        IdeasFactory.getAllIdeas().then(function(d){
+            if(!d){
+                return;
+            }else{
+                $scope.ideas = d;
+            }
+        });
     }
 
 ]);
