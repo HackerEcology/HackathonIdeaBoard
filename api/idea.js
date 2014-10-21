332a103ecf7fd2db164c813443682753b975b127
tdjackey <tdjackey@gmail.com>
2014-10-21 22:06:32 +0800

    add getIP

--- api/idea.js
+++ api/idea.js
@@ -25,6 +25,7 @@ var Idea = mongoose.model("Idea",{
 
 
 var newIdea = function(req, res, next){
+  //getClientIp(req);
 	console.log("hi newIdea~");
 	var newIdea = new Idea({
     user_id: req.param('user_id'),
@@ -58,6 +59,32 @@ var listIdea = function(req, res, next){
             return console.log(err);
         }
     });
+};
+var getClientAddress = function (req) {
+    console.log((req.headers['x-forwarded-for'] || '').split(',')[0] 
+        || req.connection.remoteAddress);
+    return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+        || req.connection.remoteAddress;
+};
+/*
+// snippet taken from http://catapulty.tumblr.com/post/8303749793/heroku-and-node-js-how-to-get-the-client-ip-address
+function getClientIp(req) {
+  var ipAddress;
+  // The request may be forwarded from local web server.
+  var forwardedIpsStr = req.header('x-forwarded-for'); 
+  if (forwardedIpsStr) {
+    // 'x-forwarded-for' header may return multiple IP addresses in
+    // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+    // the first one
+    var forwardedIps = forwardedIpsStr.split(',');
+    ipAddress = forwardedIps[0];
+  }
+  if (!ipAddress) {
+    // If request was not forwarded
+    ipAddress = req.connection.remoteAddress;
+  }
+  return ipAddress;
+};*/
 /*
   Idea.find(user_id)
     .sort({time: 'desc'})
@@ -69,7 +96,7 @@ var listIdea = function(req, res, next){
       }
     });
 */
-};
 
+exports.getIp = getClientAddress;
 exports.newIdea = newIdea;
 exports.listIdea = listIdea;
